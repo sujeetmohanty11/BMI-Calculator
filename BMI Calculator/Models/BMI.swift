444f6e5d7716be400bd2d8955aa4e83bfca078dc
Sujeet Kumar Mohanty <60059829+sujeetmohanty11@users.noreply.github.com>
2020-03-02 01:40:11 +0530

    Add files via upload

---
+++ BMI Calculator/Models/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Sujeet Kumar Mohanty on 01/03/2020.
+//  Copyright © 2020 Sujeet Kumar Mohanty. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
